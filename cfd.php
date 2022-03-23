0998f76df8face850b597b14da7f315a2535d187
RamonWebDev <ramonplaysgames@gmail.com>
2022-03-22 22:23:45 -0500

    uploading files

---
+++ cfd.php
@@ -0,0 +1,9 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "ramonm_tracker";
+$dbpassword = "n1oyf-09RH*}";
+$dbdatabase = "ramonm_expenseTracker";
+$config_basedir = "ramonmorales831.com/ExpenseTracker";
+$db = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbdatabase) or die ("Error " . mysqli_error($db));
+date_default_timezone_set('America/Chicago');
+?>
---
+++ register.php
@@ -0,0 +1,41 @@
+<?php 
+session_name('tracker');
+session_start();
+require("cfd.php");
+
+if(isset($_POST['submit'])) {
+    if($_POST['password'] == $_POST['password']){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        //creating the salt
+        $salt = hash('256', uniqid(mt_rand(), true) . 'ek2lszn456i0qtkgha' . strtolower($username));
+        
+        //add hash to password
+        $hash = $salt . $password;
+        
+        //mix the salted password a lot
+        for($i = 0; $i < 100000; $i++){
+            $hash = hash('sha256', $hash);
+        }//end for
+
+        $hash = $salt . $hash;
+        $isql = "INSERT into login(username, password) values('".$username."','".$hash."')";
+        if(mysqli_query($db, $isql)){
+            header("Location: login.php?new=true");
+        }//end if
+    }//end if
+}//end if post submit
+?>
+
+<form action="<?php echo $file;?>" method='post' id='otherlogin'>
+
+    <label for="username">Username</label>
+    <input type="text" id="username" name="username" placeholder="username" require="">
+
+    <label for="password">Passowrd</label>
+    <input type="text" id="password" name="password" placeholder="password" require="">
+
+
+
+</form>
