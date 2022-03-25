b391eef57be59b1975e0ef514b934a9fcb3c10f9
RamonWebDev <ramonplaysgames@gmail.com>
2022-03-24 22:35:49 -0500

    redoing login system

--- cfd.php
+++ cfd.php
@@ -3,7 +3,14 @@ $dbhost = "localhost";
 $dbuser = "ramonm_tracker";
 $dbpassword = "n1oyf-09RH*}";
 $dbdatabase = "ramonm_expenseTracker";
-$config_basedir = "ramonmorales831.com/ExpenseTracker";
-$db = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbdatabase) or die ("Error " . mysqli_error($db));
-date_default_timezone_set('America/Chicago');
+
+try{
+    $db = new PDO("mysql:host={$dbhost};dbname={$dbdatabase}", $dbuser, $dbpassword);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Trouble shoot exception
+}
+
+catch(PDOEXCEPTION $e){
+    echo $e->getMessage();
+}
+
 ?>
--- register.php
+++ register.php
@@ -3,50 +3,105 @@ session_name('tracker');
 session_start();
 require("cfd.php");
 
-if(isset($_POST['submit'])) {
-    if($_POST['password'] == $_POST['vpassword']){
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+if(isset($_SESSION['user'])){
+    header("location: index.php");
+}//end if
 
-        //creating the salt
-        $salt = hash('sha256', uniqid(mt_rand(), true) . 'ek2lszn456i0qtkgha' . strtolower($username));
-        
-        //add hash to password
-        $hash = $salt . $password;
-        
-        //mix the salted password a lot
-        for($i = 0; $i < 100000; $i++){
-            $hash = hash('sha256', $hash);
-        }//end for
+if(isset($_REQUEST['register_btn'])){//checks if form has been filled
+    $username = filter_var($_REQUEST['username'], FILTER_SANITIZE_STRING); //sanatizes user info
+    $email = filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
+    $password = strip_tags($_REQUEST['password']);
 
-        $hash = $salt . $hash;
-        $isql = "INSERT into login(username, password) values('".$username."','".$hash."')";
-        if(mysqli_query($db, $isql)){
-            header("Location: login.php?new=true");
-        }//end if
+    if(empty($username)){//checks if it's empty
+        $errorMsg[0][] = 'Name required';
     }//end if
-}//end if post submit
 
-if(isset($_SESSION['USERNAME'])){
-    require('header.php');
-    echo "Welcome, " . $_SESSION['USERNAME'] . ", you are logged into our system.";
-}else{
-    require('header.php');
-}
+    if(empty($email)){
+        $errorMsg[1][] = 'Email required';
+    }//end if
+
+    if(empty($password)){
+        $errorMsg[2][] = 'Passowrd required';
+    }//end if
+    
+    if(strlen($password) < 6){
+        $errorMsg[2][] = 'Passowrd is to short';
+    }//end if
+
+    if(empty($errorMsg)){
+
+        try{
+            $select_stmt = $db->prepare("SELECT username, email FROM user WHERE email = :email"); //setting up sql statement
+            $select_stmt->execute([':email' => $email]);//checks if email is already in database
+            $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
+            
+            if(isset($row['email']) == $email){
+                $errorMsg[1] = "Email address already exists. Please log in or make an account.";
+            } else {
+                $hashed_password = password_hash($password, PASSWORD_DEFAULT);//hash password
+                $created = new DateTime();//gets current time
+                $created = $created->format('Y-m-d H:i:s');//formatting time 
+
+                $insert_stmt = $db->prepare("INSERT into user(username, email, password,datecreated) VALUES (:username, :email, :password, :datecreated)");// insert query
+
+                if(
+                    $insert_stmt->execute(
+                        [
+                            ':username' => $username,
+                            ':email' => $email,
+                            ':password' => $hashed_password,
+                            ':datecreated' => $created
+                        ]
+                    )
+                ){//if true send user to different page
+                    header("location: index.php");
+                }
+            }//end else
+        }//end try
+        catch(PDOExecption $e){//Gets error if something is wrong with query
+            $pdoError = $e->getMessage();
+        }
+    }//end if
+}//end if
+
 ?>
 
-<form action="<?php echo $file;?>" method='post' id='otherlogin'>
 
+<form action="register.php" method='post'>
+
+    <?php
+    if(isset($errorMsg[0])){//echo out message if email is wrong
+        foreach($errorMsg[0] as $nameErrors){
+            echo "<p>".$nameErrors."<p>";
+        }
+    }
+    ?>
     <label for="username">Username</label>
     <input type="text" id="username" name="username" placeholder="username" require="">
+
+
+    <?php
+    if(isset($errorMsg[1])){////echo out message if something is wrong with the email
+        foreach($errorMsg[1] as $emailErrors){
+            echo "<p>".$emailErrors."<p>";
+        }
+    }
+    ?>
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" placeholder="Enter Email" require="">
+
+    <?php
+    if(isset($errorMsg[2])){//echo out message if something is wrong with the password
+        foreach($errorMsg[2] as $passwordErrors){
+            echo "<p>".$passwordErrors."<p>";
+        }
+    }
+    ?>
 
     <label for="password">Password</label>
     <input type="password" id="password" name="password" placeholder="password" require="">
 
-    <label for="password">Verify Password</label>
-    <input type="password" id="password" name="vpassword" placeholder="password" require="">
-
-    <input type="submit" id="submit" name="submit" value="Register">
+    <input type="submit" id="submit" name="register_btn" value="Register">
 
 
 
